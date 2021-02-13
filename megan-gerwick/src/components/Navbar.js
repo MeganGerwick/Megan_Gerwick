3d16d25bc9f909e86aff2b46030257538da7ceb6
Louisa May <mgerwick@gmail.com>
2021-02-13 09:57:58 -0600

    added separate pages to app

---
+++ megan-gerwick/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Navbar, Image, Nav } from 'react-bootstrap';
+import favicon from "./images/mgfavi.png";
+
+<Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">
+        <Image src={favicon} width={50} height={50} />
+  Megan Gerwick</Navbar.Brand>
+
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav.Link href="#About">About Me</Nav.Link>
+        <Nav.Link href="#Skills">Skills and Technologies</Nav.Link>
+        <Nav.Link href="#Projects">Projects</Nav.Link>
+        <Nav.Link href="#Contact">Contact</Nav.Link>
+    </Navbar.Collapse>
+</Navbar>
