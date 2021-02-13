0f429eaf0bd2263fd64260a9806fc8da03073745
Louisa May <mgerwick@gmail.com>
2021-02-13 11:10:56 -0600

    added Navigation bar to necessary pages

--- megan-gerwick/src/components/Navbar.js
+++ megan-gerwick/src/components/Navbar.js
@@ -1,16 +1,20 @@
 import { Navbar, Image, Nav } from 'react-bootstrap';
 import favicon from "./images/mgfavi.png";
 
-<Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">
-        <Image src={favicon} width={50} height={50} />
+export default function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">
+                <Image src={favicon} width={50} height={50} />
   Megan Gerwick</Navbar.Brand>
 
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-        <Nav.Link href="#About">About Me</Nav.Link>
-        <Nav.Link href="#Skills">Skills and Technologies</Nav.Link>
-        <Nav.Link href="#Projects">Projects</Nav.Link>
-        <Nav.Link href="#Contact">Contact</Nav.Link>
-    </Navbar.Collapse>
-</Navbar>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav.Link href="#About">About Me</Nav.Link>
+                <Nav.Link href="#Skills">Skills and Technologies</Nav.Link>
+                <Nav.Link href="#Projects">Projects</Nav.Link>
+                <Nav.Link href="#Contact">Contact</Nav.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+};
